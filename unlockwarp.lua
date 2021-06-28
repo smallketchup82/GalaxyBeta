fda6ebc0ec47bca679d28571edc5bf6296658726
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2021-06-28 19:00:00 -0400

    fix

--- unlockwarp.lua
+++ unlockwarp.lua
@@ -1,3 +1,7 @@
+local YourTeam = game.Players.LocalPlayer.Team
+local YourMiner = game.Players.LocalPlayer.ActiveShip.Value
+local ship = workspace.Ships:FindFirstChild(tostring(YourTeam)):FindFirstChild(tostring(YourMiner))
+
 if ship.Configuration:FindFirstChild("NoWarp") then
     ship.Configuration.NoWarp.Value = false 
 else
