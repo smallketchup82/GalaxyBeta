e88206ad16ccc1957782acdb42e9d1381cc96188
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2021-06-28 19:01:52 -0400

    fix

--- unlockwarp.lua
+++ unlockwarp.lua
@@ -2,8 +2,8 @@ local YourTeam = game.Players.LocalPlayer.Team
 local YourMiner = game.Players.LocalPlayer.ActiveShip.Value
 local ship = workspace.Ships:FindFirstChild(tostring(YourTeam)):FindFirstChild(tostring(YourMiner))
 
-if ship.Configuration:FindFirstChild("NoWarp") then
-    ship.Configuration.NoWarp.Value = false 
+if ship.Configuration:FindFirstChild("NoWarp") ~= nil then
+    ship.Configuration.NoWarp.Value = false
 else
     print("Ship already has warp enabled")
 end
