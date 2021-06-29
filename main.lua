040a44b4198b1dca6ae1e18c588bf2492791c1cc
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2021-06-28 21:58:37 -0400

    f

--- main.lua
+++ main.lua
@@ -3,5 +3,5 @@ local UILibrary = loadstring(game:HttpGet("https://pastebin.com/raw/V1ca2q9s"))(
 local MainUI = UILibrary.Load("Roblox Galaxy Beta Utilities")
 local mainpage = MainUI.AddPage("Main", false)
 local farmingpage = MainUI.AddPage("Autofarm", true)
-local 
-local teleportpage = MenuUI.
+local combatpage = MainUI.AddPage("Combat", true)
+local teleportpage = MenuUI.AddPage("Teleports", false)
